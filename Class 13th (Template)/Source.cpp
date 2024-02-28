55feba15a980daba93df0ba0b4ac3d480bdfc04b
jun-s1016 <153084343+jun-s1016@users.noreply.github.com>
2024-02-28 21:04:37 +0900

    2024.02.28 가상 상속

---
+++ Class 13th (Template)/Source.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include "../Program/LinkedList.h"
+#include "../Program/Bank.h"
+
+using namespace std;
+
+template <typename T>
+void Calculator(T x, T y)
+{
+	cout << " x + y  : " << x + y << endl;
+}
+
+template <>
+void Calculator(char x, char y)
+{
+	cout << "x의 값 : " << x << endl;
+	cout << "y의 값 : " << y << endl;
+}
+
+int main()
+{
+#pragma region 템플릿 
+	// 데이터 형식에 의존하지 않고, 하나의 값이
+	// 여러 다른 데이터 타입들을 가질 수 있는 기술에 중점을 두어
+	// 재사용성을 높일 수 있는 기능 
+	/*Calculator(10, 20);
+	Calculator(10.5f, 28.3f);
+	Calculator('A','B');
+
+	LinkedList<int> linkedList;
+
+	linkedList.Push(1000);
+
+	cout << linkedList.GetData() << endl;*/
+
+
+#pragma endregion
+
+#pragma region Friend
+	// Friend 로 선언된 클래스의 private 및 protected 멤버에
+	// 접근할 수 있도록 설정해주는 기능.
+
+	/*Character character;
+	character.Show();
+
+	Bank bank;
+	bank.Withdrawal(character, 15000);
+
+	bank.Show();
+	character.Show();*/
+
+
+#pragma endregion
+
+#pragma region 등비수열
+
+
+	//int firstTerm, different, n;
+	//cout << " 초항 : ";
+	//cin >> firstTerm;
+	//cout << " 공비 : ";
+	//cin >> different;
+	//cout << " 횟수 : ";
+	//cin >> n;
+	//for (int i = 1; i < n+1; i++)
+	//{
+	//	cout << firstTerm << " ";
+	//
+	//	firstTerm *= different;
+	//}
+
+#pragma endregion
+
+	return 0;
+}
---
+++ Class 14th (Virtual Inheritance)/Source.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Food
+{
+public:
+	Food()
+	{
+		cout << "Create Food" << endl;
+	}
+	~Food()
+	{
+		cout << "Release Food" << endl;
+	}
+};
+
+class Meat : public virtual Food
+{
+public:
+	Meat()
+	{
+		cout << "Create Meat" << endl;
+	}
+	~Meat()
+	{
+		cout << "Release Meat" << endl;
+	}
+};
+
+class Vegetable : public virtual Food
+{
+public:
+	Vegetable()
+	{
+		cout << "Create Vegetable" << endl;
+	}
+	~Vegetable()
+	{
+		cout << "Release Vegetable" << endl;
+
+	}
+};
+
+class Soup : public Meat, public Vegetable
+{
+public:
+	Soup()
+	{
+		cout << "Create Soup" << endl;
+
+	}
+	~Soup()
+	{
+		cout << "Release Soup" << endl;
+
+	}
+};
+
+int main()
+{
+#pragma region 가상 상속
+
+	//Soup soup;
+
+#pragma endregion
+
+#pragma region 등차수열
+
+
+	/*int initial,different,n;
+
+	cout << "초항 : ";
+	cin >> initial;
+	cout << "공차 : ";
+	cin >> different;
+	cout << "횟수 : ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << initial << " ";
+		initial += different;
+	}*/
+
+#pragma endregion
+
+	return 0;
+}
